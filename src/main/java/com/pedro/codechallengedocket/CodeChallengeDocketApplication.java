9dd9ebb7723ff521637d1c421f2c51254f53d434
Matheus Pedro <matheus.pedro1214@gmail.com>
2021-04-19 21:07:21 -0300

    Criacao do Projeto

---
+++ src/main/java/com/pedro/codechallengedocket/CodeChallengeDocketApplication.java
@@ -0,0 +1,13 @@
+package com.pedro.codechallengedocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeChallengeDocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeChallengeDocketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedro/codechallengedocket/CodeChallengeDocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedro.codechallengedocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeChallengeDocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
